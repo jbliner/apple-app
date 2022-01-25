39c501bfc822cedee22dbf1bf9bb51dca2be8752
jbliner <jb0147@mix.wvu.edu>
2022-01-25 18:42:58 -0500

    edit3

--- src/app/header/topbar.component.ts
+++ src/app/header/topbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "apple-topbar",
-    templateUrl: "topbar.component.html"
+    templateUrl: "topbar.component.html",
+    styleUrls: ["topbar.component.css"]
 })
 
 export class TopBarComponent{
