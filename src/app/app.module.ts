2df2f078d8c464d60d34ce4381378a1a88af925f
jbliner <jb0147@mix.wvu.edu>
2022-02-01 11:56:47 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './header/topbar.component';
+import { TopBarComponent2 } from './secondheader/topbar2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    TopBarComponent2
   ],
   imports: [
     BrowserModule
---
+++ src/app/secondheader/topbar2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "apple-topbar2",
+    templateUrl: "topbar2.component.html",
+    styleUrls: ["topbar2.component.css"]
+})
+
+export class TopBarComponent2{
+
+}
