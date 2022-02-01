b6fd27a85a967bf17559346e9b73cee1bbbe4833
jbliner <jb0147@mix.wvu.edu>
2022-02-01 15:00:34 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './header/topbar.component';
 import { TopBarComponent2 } from './secondheader/topbar2.component';
+import { TopBarComponent3 } from './thirdheader/topbar3.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    TopBarComponent2
+    TopBarComponent2,
+    TopBarComponent3
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
