87ca67ad5318af579e5bdfd17bffe5d7cee7c488
jbliner <jb0147@mix.wvu.edu>
2022-02-01 14:17:12 -0500

    additional components

---
+++ src/app/thirdheader/topbar3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "apple-topbar3",
+    templateUrl: "topbar3.component.html",
+    styleUrls: ["topbar3.component.css"]
+})
+
+export class TopBarComponent3{
+
+}
